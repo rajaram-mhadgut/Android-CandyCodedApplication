51f226f93e47fec0e867361a093c396f1a4cf233
rajaram-mhadgut <rajaram.mhadgut@users.noreply.github.com>
2019-03-14 00:17:56 +0530

    Completed phone module

--- CandyCoded/app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ CandyCoded/app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -26,7 +26,7 @@ public class InfoActivity extends AppCompatActivity {
     }
 
     // ***
-    // TODO - Task 2 - Launch the Google Maps Activity
+    // COMPLETED - Task 2 - Launch the Google Maps Activity
     // ***
     public void createMapIntent(View view){
         Uri uriAddress = Uri.parse("geo:0,0?q=618 E South St Orlando, FL 32801");
@@ -40,6 +40,12 @@ public class InfoActivity extends AppCompatActivity {
     }
 
     // ***
-    // TODO - Task 3 - Launch the Phone Activity
+    // COMPLETED - Task 3 - Launch the Phone Activity
     // ***
+
+    public void createPhoneIntent(View view){
+        Intent phoneIntent = new Intent(Intent.ACTION_DIAL);
+        phoneIntent.setData(Uri.parse("tel:0123456789"));
+        startActivity(phoneIntent);
+    }
 }
