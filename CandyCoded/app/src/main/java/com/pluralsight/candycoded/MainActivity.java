c8cbeeac951884e58d60082f71d0a5d9c71d15cc
rajaram-mhadgut <rajaram.mhadgut@users.noreply.github.com>
2019-03-14 00:04:23 +0530

    Completed google map module

--- CandyCoded/app/src/main/java/com/pluralsight/candycoded/MainActivity.java
+++ CandyCoded/app/src/main/java/com/pluralsight/candycoded/MainActivity.java
@@ -80,11 +80,11 @@ public class MainActivity extends AppCompatActivity {
         inflater.inflate(R.menu.main, menu);
         return true;
     }
-    // ***
-    // TODO - Task 1 - Show Store Information Activity
-    // ***
 
 
+    // ***
+    // COMPLETED - Task 1 - Show Store Information Activity
+    // ***
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         Intent infoIntent = new Intent(this, InfoActivity.class);
